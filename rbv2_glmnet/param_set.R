77bf98633a7a8a5187b4b172aedfb235c061211b
Lennart Schneider <lennart.sch@web.de>
2022-01-06 17:01:27 +0100

    add search_spaces for all except fcnet

--- rbv2_glmnet/param_set.R
+++ rbv2_glmnet/param_set.R
@@ -7,11 +7,33 @@ codomain = ps(
   timepredict = p_dbl(lower = 0, upper = Inf, tags = "minimize")
 )
 
+search_space = ps(
+  alpha = p_dbl(lower = 0, upper = 1),
+  s = p_dbl(lower = -7, upper = 7, tags = "log", trafo = function(x) exp(x)),
+  trainsize = p_dbl(lower = 0.05, upper = 1, tags = "budget"),
+  repl = p_int(lower = 1L, upper = 10L, tags = "budget"),
+  num.impute.selected.cpo = p_fct(levels = c("impute.mean", "impute.median", "impute.hist")),
+  task_id = p_fct(levels = c("1040", "1049", "1050", "1053", "1056", "1063", "1067", "1068",
+    "11", "1111", "12", "14", "1461", "1462", "1464", "1468", "1475",
+    "1476", "1478", "1479", "1480", "1485", "1486", "1487", "1489",
+    "1494", "1497", "15", "1501", "1510", "1515", "1590", "16", "18",
+    "181", "182", "188", "22", "23", "23381", "23512", "24", "28",
+    "29", "3", "307", "31", "312", "32", "334", "37", "375", "377",
+    "38", "40496", "40498", "40499", "40536", "40668", "40670", "40701",
+    "40900", "40966", "40975", "40978", "40979", "40981", "40982",
+    "40983", "40984", "40994", "41138", "41142", "41143", "41146",
+    "41156", "41157", "41159", "41161", "41162", "41212", "41278",
+    "4134", "4135", "4154", "42", "44", "4534", "4538", "4541", "458",
+    "46", "469", "470", "50", "54", "60", "6332")
+    , tags = "task_id"
+  )
+)
+
 domain = ps(
-  alpha = p_dbl(lower = 0, upper = 1, default = 1),
-  s = p_dbl(lower = -7, upper = 7, default = 0, tags = "log", trafo = function(x) exp(x)),
-  trainsize = p_dbl(lower = 0.05, upper = 1, tag = "budget"),
-  repl = p_int(lower = 1, upper = 10, tag = "budget"),
+  alpha = p_dbl(lower = 0, upper = 1),
+  s = p_dbl(lower = exp(-7), upper = exp(7)),
+  trainsize = p_dbl(lower = 0.05, upper = 1, tags = "budget"),
+  repl = p_int(lower = 1L, upper = 10L, tags = "budget"),
   num.impute.selected.cpo = p_fct(levels = c("impute.mean", "impute.median", "impute.hist")),
   task_id = p_fct(levels = c("1040", "1049", "1050", "1053", "1056", "1063", "1067", "1068",
     "11", "1111", "12", "14", "1461", "1462", "1464", "1468", "1475",
--- rbv2_ranger/param_set.R
+++ rbv2_ranger/param_set.R
@@ -7,17 +7,17 @@ codomain = ps(
   timepredict = p_dbl(lower = 0, upper = Inf, tags = "minimize")
 )
 
-domain = ps(
-  num.trees = p_int(lower = 1, upper = 2000),
+search_space = ps(
+  num.trees = p_int(lower = 1L, upper = 2000L),
   # replace = p_lgl(),
   sample.fraction = p_dbl(lower = 0.1, upper = 1),
   mtry.power = p_int(lower = 0, upper = 1),
   respect.unordered.factors = p_fct(levels = c("ignore", "order", "partition")),
-  min.node.size = p_int(lower = 1, upper = 100),
+  min.node.size = p_int(lower = 1L, upper = 100L),
   splitrule = p_fct(levels = c("gini", "extratrees")),
-  num.random.splits = p_int(lower = 1, upper = 100, default = 1L, depends = splitrule == "extratrees"),
-  trainsize = p_dbl(lower = 0.05, upper = 1, tag = "budget"),
-  repl = p_int(lower = 1, upper = 10, tag = "budget"),
+  num.random.splits = p_int(lower = 1L, upper = 100L, depends = splitrule == "extratrees"),
+  trainsize = p_dbl(lower = 0.05, upper = 1, tags = "budget"),
+  repl = p_int(lower = 1L, upper = 10L, tags = "budget"),
   num.impute.selected.cpo = p_fct(levels = c("impute.mean", "impute.median", "impute.hist")),
   task_id = p_fct(levels = c("1040", "1049", "1050", "1053", "1056", "1063", "1067", "1068",
     "11", "1111", "12", "1220", "14", "1457", "1461", "1462", "1464",
@@ -33,6 +33,36 @@ domain = ps(
     "41162", "41163", "41164", "41165", "41166", "41168", "41212",
     "41216", "41278", "4134", "4135", "4154", "42", "44", "4534",
     "4538", "4541", "458", "46", "469", "470", "50", "54", "6", "60",
-    "6332")
-    , tags = "task_id")
+    "6332"), tags = "task_id"
+  )
+)
+
+domain = ps(
+  num.trees = p_int(lower = 1L, upper = 2000L),
+  # replace = p_lgl(),
+  sample.fraction = p_dbl(lower = 0.1, upper = 1),
+  mtry.power = p_int(lower = 0, upper = 1),
+  respect.unordered.factors = p_fct(levels = c("ignore", "order", "partition")),
+  min.node.size = p_int(lower = 1L, upper = 100L),
+  splitrule = p_fct(levels = c("gini", "extratrees")),
+  num.random.splits = p_int(lower = 1L, upper = 100L, depends = splitrule == "extratrees"),
+  trainsize = p_dbl(lower = 0.05, upper = 1, tags = "budget"),
+  repl = p_int(lower = 1L, upper = 10L, tags = "budget"),
+  num.impute.selected.cpo = p_fct(levels = c("impute.mean", "impute.median", "impute.hist")),
+  task_id = p_fct(levels = c("1040", "1049", "1050", "1053", "1056", "1063", "1067", "1068",
+    "11", "1111", "12", "1220", "14", "1457", "1461", "1462", "1464",
+    "1468", "1475", "1476", "1478", "1479", "1480", "1485", "1486",
+    "1487", "1489", "1494", "1497", "15", "1501", "151", "1510",
+    "1515", "1590", "16", "18", "181", "182", "188", "22", "23",
+    "23381", "23512", "23517", "24", "28", "29", "3", "300", "307",
+    "31", "312", "32", "334", "37", "375", "377", "38", "40496",
+    "40498", "40499", "40536", "40668", "40670", "40685", "40701",
+    "40900", "40927", "40966", "40975", "40978", "40979", "40981",
+    "40982", "40983", "40984", "40994", "41027", "41138", "41142",
+    "41143", "41146", "41150", "41156", "41157", "41159", "41161",
+    "41162", "41163", "41164", "41165", "41166", "41168", "41212",
+    "41216", "41278", "4134", "4135", "4154", "42", "44", "4534",
+    "4538", "4541", "458", "46", "469", "470", "50", "54", "6", "60",
+    "6332"), tags = "task_id"
+  )
 )
