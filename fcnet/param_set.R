59138d2a2c4dd024488c973e9453cd69e83d31c7
Florian <pfistfl@users.noreply.github.com>
2022-02-16 11:41:32 +0100

    Update param_set.R

--- fcnet/param_set.R
+++ fcnet/param_set.R
@@ -1,9 +1,9 @@
 domain = ps(
-  task = p_fct(levels = c("fcnet_naval_propulsion", "fcnet_protein_structure", "fcnet_slice_localization","fcnet_parkinsons_telemonitoring")),
+  task = p_fct(levels = c("fcnet_naval_propulsion", "fcnet_protein_structure", "fcnet_slice_localization","fcnet_parkinsons_telemonitoring"), tags = "task_id"),
   activation_fn_1 = p_fct(levels = c("relu", "tanh")),
   activation_fn_2 = p_fct(levels = c("relu", "tanh")),
   lr_schedule = p_fct(levels = c("const", "cosine")),
-  epoch = p_dbl(lower = 0, upper = 4.605, tags = c("int", "log"), trafo = function(x) as.integer(round(exp(x)))),
+  epoch = p_dbl(lower = 0, upper = 4.605, tags = c("int", "log"), trafo = function(x) as.integer(round(exp(x))), tags = "budget"),
   replication = p_int(lower = 1, upper = 4),
   batch_size = p_dbl(lower = 2.08, upper = 4.159, tags = c("int", "log"), trafo = function(x) as.integer(round(exp(x)))),
   init_lr = p_dbl(lower = -7.6, upper = -2.3, tags = "log", trafo = function(x) exp(x)),
